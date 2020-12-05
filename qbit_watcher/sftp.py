a3ea5a39ab496292eefa2aae4b17a94c7b4e197a
fdugast <dugast.fabien@gmail.com>
2020-12-05 15:28:14 +0100

    Add progress download when retrieving a file

--- qbit_watcher/sftp.py
+++ qbit_watcher/sftp.py
@@ -6,12 +6,23 @@ from pathlib import Path
 
 LOGGER = logging.getLogger(__name__)
 
+dict_pgr = {}
+percent_pgr = 10
+
+def init_progress():
+    for i in range(0, 101):
+        if i % percent_pgr == 0:
+            dict_pgr[str(i)] = ""
+
+init_progress()
+
 class TorrentSftp:
     def __init__(self, conf, dest_folder, toaster):
         self.conf = conf
         self.dest = dest_folder
         self.sftp = self.get_client()
         self.toaster = toaster
+
 
     def get_client(self):
         cnopts = CnOpts()
@@ -32,7 +43,11 @@ class TorrentSftp:
         """
         LOGGER.info("Retrieving '%s'" % fname)
         if self.sftp.isfile(fname):
-            self.sftp.get(fname, localpath="%s/%s" %(self.dest, fname))
+            self.sftp.get(
+                fname,
+                localpath="%s/%s" % (self.dest, fname),
+                callback=lambda x,y: TorrentSftp.progress_cb(x, y)
+            )
         elif self.sftp.isdir(fname):
             # Cannot get folder if its already exists on local.
             # Therefore remove it before downloading again
@@ -43,7 +58,16 @@ class TorrentSftp:
         else:
             LOGGER.info("No file to download found")
             return
+        LOGGER.info("%s is downloaded", fname)
         self.toaster.notif("%s is downloaded" % (fname))
+
+    @staticmethod
+    def progress_cb(x, y):
+        current_mb = x / 1024 / 1024
+        total_mb = y / 1024 / 1024
+        if int(100*(int(current_mb)/int(total_mb))) % percent_pgr == 0 and dict_pgr[str(int(100*(int(current_mb)/int(total_mb))))] == "":
+            LOGGER.info("{}% ({} / {} MB)".format(str("%d" % (100*(int(x)/int(y)))), int(current_mb), int(total_mb)))
+            dict_pgr[str(int(100*(int(current_mb)/int(total_mb))))] = "1"
 
     def close(self):
         self.sftp.close()
